eee3140bcddd545dcb7674e85f637051e95a53fe
Louis Fisher <37254732+LFISHER7@users.noreply.github.com>
2022-04-27 11:48:17 +0100

    update config and create creatinine notebook

--- analysis/config_numeric_value_checks.py
+++ analysis/config_numeric_value_checks.py
@@ -1,6 +1,6 @@
 
 # enter codelist name as a string
-codelist_name = 'opensafely-ast'
+codelist_name = 'creatinine'
 
 # specify headers for code column and term (description) column in dataset
 # These should be strings
@@ -10,11 +10,11 @@ term_column='term'
 
 # enter year 
 # (must be a four-digit numeric value e.g. 2020)
-year = 2021
+year = 2022
 
 # enter expected lower and upper bounds for range of values. 
 # Use positive integers
 # Note zeros will be treated as missing.
-lower = 1
-upper = 100
+lower = 50
+upper = 120
 
